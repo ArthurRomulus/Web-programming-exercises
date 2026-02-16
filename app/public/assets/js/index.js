7b2f059e4c9ece333edd31fa6bdae7d1ce00ae34
ArthurRomulus <miguepg06@gmail.com>
2026-02-16 11:41:37 -0600

    update 12

---
+++ app/public/assets/js/index.js
@@ -0,0 +1,12 @@
+const app = {
+    urlDatos : "/resources/data/autos.json",
+    filtro : "todos",
+
+    cargarFichas : function () {
+        const fichas = document.querySelector("#fichas");
+
+
+    },
+    
+};
+
