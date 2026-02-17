a70e2a83a1a51d3697c58593209ca81264661fbe
ArthurRomulus <miguepg06@gmail.com>
2026-02-17 11:47:24 -0600

    Update 14

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -8,7 +8,20 @@ const app = {
         fetch(this.urlDatos).then(response => response.json).then(autos => {
             for (let auto of autos){
                 if (filtro === "todos" || auto.tipo === "filtro"){
-                    
+                    html += `
+                    <div class="ficha">
+                        <img src="${auto.foto}" alt="${auto.modelo}">
+                        <div class="datos">
+                            <h3>${auto.marca}</h3>
+                            <span>${auto.modelo}</span>
+                            <span>${auto.anio}</span>
+                            <br>
+                            <small>
+                            ${auto.desplazamiento}, ${auto.potencia},  ${auto.rendimiento}
+                            </small>
+                        </div>
+                        </div>
+                    `;
                 }
             }
         }).catch( err => console.error (err));
