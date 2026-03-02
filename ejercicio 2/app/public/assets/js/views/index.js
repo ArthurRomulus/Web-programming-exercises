97112c1ab07e8079bd1af1564b2d9e0fcad1e786
ArthurRomulus <miguepg06@gmail.com>
2026-03-02 10:39:47 -0600

    update 28

--- ejercicio 2/app/public/assets/js/views/index.js
+++ ejercicio 2/app/public/assets/js/views/index.js
@@ -5,4 +5,8 @@ const app = {
   urlComments : "https://jsonplaceholder.typicode.com/comments",
   urlUsers : "https://jsonplaceholder.typicode.com/users",
 
+
+  //variables de estado
+
+  /**Cargar las publicaciones (posts) desde la API y mostrarlas mediante DOM */
 };
