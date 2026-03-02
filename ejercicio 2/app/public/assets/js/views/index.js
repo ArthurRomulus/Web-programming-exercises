dab681a610904f92dc2c2710c8312acbb8f2cf8e
ArthurRomulus <miguepg06@gmail.com>
2026-03-02 10:37:59 -0600

    update 27

--- ejercicio 2/app/public/assets/js/views/index.js
+++ ejercicio 2/app/public/assets/js/views/index.js
@@ -1,6 +1,8 @@
 const app = {
   //URL's base para las APIs
   
-  urlPosts : "",
+  urlPosts : "https://jsonplaceholder.typicode.com/posts",
+  urlComments : "https://jsonplaceholder.typicode.com/comments",
+  urlUsers : "https://jsonplaceholder.typicode.com/users",
 
 };
