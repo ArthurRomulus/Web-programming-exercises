933231ac456307fa529d0ec6be006d2bc36431a7
ArthurRomulus <miguepg06@gmail.com>
2026-03-02 10:30:37 -0600

    update 24

--- ejercicio 2/app/resources/views/index.php
+++ ejercicio 2/app/resources/views/index.php
@@ -4,6 +4,25 @@
     include "../layouts/main_footer.php";
 
     head();
+
+?>
+
+<div class="row mx-auto mt-5" style="">
+    <div class="col-8 shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="content" id="content">
+            <!-- Publicaciones [API JSON] -->
+
+        </div>
+    </div>
+    <div class="col-4 shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="autores list-group" id="autores">
+            
+        </div>
+    </div>
+</div>
+
+<?php
+    
     foot();
 
 ?>
