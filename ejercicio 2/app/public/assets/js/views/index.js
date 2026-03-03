ffd7f7550feeb8d558b1084d28de3385c6157e71
ArthurRomulus <miguepg06@gmail.com>
2026-03-03 12:35:59 -0600

    update 42

--- ejercicio 2/app/public/assets/js/views/index.js
+++ ejercicio 2/app/public/assets/js/views/index.js
@@ -1,57 +1,96 @@
-const app = {
-  //URL's base para las APIs
-  
-  urlPosts : "https://jsonplaceholder.typicode.com/posts",
-  urlComments : "https://jsonplaceholder.typicode.com/comments",
-  urlUsers : "https://jsonplaceholder.typicode.com/users",
-  //variables de estado
+    const app = {
+    //URLs base apra las APIs
+    urlPosts : "https://jsonplaceholder.typicode.com/posts",
+    urlComments : "https://jsonplaceholder.typicode.com/comments",
+    urlUsers : "https://jsonplaceholder.typicode.com/users",
 
-  /**Cargar las publicaciones (posts) desde la API y mostrarlas mediante DOM */
+    //Variables de estado 
 
-  loadCommets : async function (params) {
-  },
+    //**Cargar las publicaciones (post) desde la API y mostrarla mediante DOM */
+    loadComments: async function(postId){},
 
-  loadPosts : async function (){
-        try{
-            //const cont = document.querySelector("#content");
-            const $cont = $("#content");
-            $cont
-                .css("width", "100%",)
-                .addClass("mx-auto mt-5");
-
-            let html = "";
-            const users = await (fetch(this.urlUsers).then(resp => resp.json()));
-            const posts = await (fetch(this.urlPosts).then(resp => resp.json()));
+    loadAuthors: async function () {
+        try {
+            const posts = await fetch(this.urlPosts).then(resp => resp.json() )
+            const users = await fetch(this.urlUsers).then(resp => resp.json() )
             posts.forEach(post => {
-                const user = users.find(user => user.id === post.userid);
-                html += `                  
-        <div class="card mb-3" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${post.title}</h5>
-                <p class="card-text">${post.body}.</p>
-            </div>
-            <div class="card-footer text-body-secondary">
-                <button class="btn btn-link" type="button" id="btn-ver-com" onclick="app.loadCommets()">
-                    Ver comentarios
-                </button>
-                <button class="btn btn-link d-none" type="button" id="btn-quitar-com" onclick="app.loadCommets()">
-                    Quitar comentarios
-                </button>
-            </div>
-        </div>
-            `;
-            });
-            $cont.html(html);
-        } catch (err){
-            console.error("Se produjo un error al cargar las publicaciones", err);
+                const user = users.find(user => user.id === post.userId) //filtrar al usuario autor de la publicacion
+                console.log(user);
+
+                html = `<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Dark card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>`;
+            })
+
+        
+        } catch (err) {
+                console.error("Error al cargar los posts:", err);
         }
-  }, 
+            
+    },
 
-  loadUsers : async function(){
-  },
+    loadPosts: async function(){
+        try {
+            //const cont = document.querySelector("#content")
+            const $cont = $("#content")
+            $cont
+                .css("width", "100%")
+                .addClass("mx-auto mt-5")
+            let html = ""
+            const posts = await fetch(this.urlPosts).then(resp => resp.json() )
+            const users = await fetch(this.urlUsers).then(resp => resp.json() )
 
-};
 
-$(function ( ){
-    app.loadPosts();
+            posts.forEach(post  => {
+                const user = users.find(user => user.id === post.userId) //filtrar al usuario autor de la publicacion
+                html += `
+                       <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${post.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">${user.id}</h6>
+                            <p class="card-text">${post.body}</p>
+                        </div>
+                        <div class="card-footer text-body-secondary">
+                            <button class="btn btn-link" type="button"
+                                id="btn-ver-com${post.id}"
+                                onclick="app.loadComments(${post.id})">
+                                Ver comentarios <i class="bi bi-chevron-down"></i> 
+                            </button>
+                            <button class="btn btn-link d-none" type="button"
+                                id="btn-ocultar-com${post.id}"
+                                onclick="app.closeComments(${post.id})">
+                                Ocultar comentarios <i class="bi bi-chevron-up"></i> 
+                            </button>
+                            <div class="card d-none" id="card-com${post.id}">
+                                <ul class="list-group list-group-flush" id="comments-${post.id}">
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+
+                `
+            })
+            $cont.html(html)
+
+            
+
+    } catch (err) {
+            console.error("Error al cargar los posts:", err);
+    }
+    },
+    loadUsers : async function(){
+    },
+
+
+
+}
+$(function(){
+    app.loadPosts()
 })
+
+app.loadAuthors();
--- ejercicio 2/app/resources/layouts/main_footer.php
+++ ejercicio 2/app/resources/layouts/main_footer.php
@@ -6,7 +6,7 @@
 ?>
 
 </div>
-    <script src="/public/assets/js/lib/jquery.min.js.js"></script>
+    <script src="/public/assets/js/lib/jquery.min.js"></script>
     <script src="/public/assets/js/lib/bootstrap.bundle.min.js"></script>
     <?php foreach ($scripts as $script) { ?>
             <script src="/public/assets/js/views/<?= $script?>"></script>
