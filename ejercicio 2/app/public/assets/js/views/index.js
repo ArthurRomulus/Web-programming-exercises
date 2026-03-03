775653fa1673bac4ef7329cf2ec7f34a8fa05063
ArthurRomulus <miguepg06@gmail.com>
2026-03-03 11:15:32 -0600

    update 39

--- ejercicio 2/app/public/assets/js/views/index.js
+++ ejercicio 2/app/public/assets/js/views/index.js
@@ -24,7 +24,14 @@ const app = {
             const commets = await(fetch(this.urlComments).then(resp => resp.json()));
             posts.forEach(post => {
                 html += `
-                
+                            <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${post.title}</h5>
+                                <p class="card-text">${post.body}.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>
             `;
             });
         } catch (err){
