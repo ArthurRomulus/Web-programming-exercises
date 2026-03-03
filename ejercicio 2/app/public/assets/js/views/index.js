1837979514ed66196a134659d81455994f472e0f
ArthurRomulus <miguepg06@gmail.com>
2026-03-03 12:07:22 -0600

    update 41

--- ejercicio 2/app/public/assets/js/views/index.js
+++ ejercicio 2/app/public/assets/js/views/index.js
@@ -4,13 +4,14 @@ const app = {
   urlPosts : "https://jsonplaceholder.typicode.com/posts",
   urlComments : "https://jsonplaceholder.typicode.com/comments",
   urlUsers : "https://jsonplaceholder.typicode.com/users",
-
-
   //variables de estado
 
   /**Cargar las publicaciones (posts) desde la API y mostrarlas mediante DOM */
 
-  loadPosts : function (){
+  loadCommets : async function (params) {
+  },
+
+  loadPosts : async function (){
         try{
             //const cont = document.querySelector("#content");
             const $cont = $("#content");
@@ -19,26 +20,38 @@ const app = {
                 .addClass("mx-auto mt-5");
 
             let html = "";
-            const users = await(fetch(this.urlUsers).then(resp => json()));
-            const posts = await(fetch(this.urlPosts).then(resp => resp.json()));
-            const commets = await(fetch(this.urlComments).then(resp => resp.json()));
+            const users = await (fetch(this.urlUsers).then(resp => resp.json()));
+            const posts = await (fetch(this.urlPosts).then(resp => resp.json()));
             posts.forEach(post => {
-                html += `
-                            <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${post.title}</h5>
-                                <p class="card-text">${post.body}.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                            </div>
+                const user = users.find(user => user.id === post.userid);
+                html += `                  
+        <div class="card mb-3" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">${post.body}.</p>
+            </div>
+            <div class="card-footer text-body-secondary">
+                <button class="btn btn-link" type="button" id="btn-ver-com" onclick="app.loadCommets()">
+                    Ver comentarios
+                </button>
+                <button class="btn btn-link d-none" type="button" id="btn-quitar-com" onclick="app.loadCommets()">
+                    Quitar comentarios
+                </button>
+            </div>
+        </div>
             `;
             });
+            $cont.html(html);
         } catch (err){
             console.error("Se produjo un error al cargar las publicaciones", err);
         }
-  }
+  }, 
+
+  loadUsers : async function(){
+  },
 
 };
 
-app.loadPosts()
+$(function ( ){
+    app.loadPosts();
+})
--- ejercicio 2/app/resources/layouts/main_footer.php
+++ ejercicio 2/app/resources/layouts/main_footer.php
@@ -1,6 +1,6 @@
 <?php
 
-    function foot(){
+    function foot($scripts = []){
 
 
 ?>
@@ -8,8 +8,8 @@
 </div>
     <script src="/public/assets/js/lib/jquery.min.js.js"></script>
     <script src="/public/assets/js/lib/bootstrap.bundle.min.js"></script>
-    <?php foreach ($script as $script) {?>
-            <script src="/public/assets/js/views/<?=$script?>"></script>
+    <?php foreach ($scripts as $script) { ?>
+            <script src="/public/assets/js/views/<?= $script?>"></script>
     <?php
     } ?>
 </body>
