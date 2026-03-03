4611ca52c39c898fe9f7d3795046a58b78414ec0
ArthurRomulus <miguepg06@gmail.com>
2026-03-03 11:15:21 -0600

    update 38

--- ejercicio 2/app/resources/layouts/main_footer.php
+++ ejercicio 2/app/resources/layouts/main_footer.php
@@ -8,7 +8,10 @@
 </div>
     <script src="/public/assets/js/lib/jquery.min.js.js"></script>
     <script src="/public/assets/js/lib/bootstrap.bundle.min.js"></script>
-    <script src="/public/assets/js/views/index.js"></script>
+    <?php foreach ($script as $script) {?>
+            <script src="/public/assets/js/views/<?=$script?>"></script>
+    <?php
+    } ?>
 </body>
 </html>
 <?php
