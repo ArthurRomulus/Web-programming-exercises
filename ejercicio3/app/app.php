b73bf115255baa6a6b4a8afd6b02b89ebaa846bc
ArthurRomulus <miguepg06@gmail.com>
2026-03-10 01:27:01 -0600

    update 46

---
+++ ejercicio3/app/app.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace app;
+    error_reporting(E_ALL);
+    init_set('display_errors', 1);
+
+    class App{
+        public function __construct(){
+            
+        }
+    }
+?>
